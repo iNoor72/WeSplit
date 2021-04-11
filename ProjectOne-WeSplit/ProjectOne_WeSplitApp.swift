b6516206d74fe1015d0bb0f9de7bad7e6b33fc7e
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-04-11 22:48:19 +0200

    Initial Commit

---
+++ ProjectOne-WeSplit/ProjectOne_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOne_WeSplitApp.swift
+//  ProjectOne-WeSplit
+//
+//  Created by Noor Walid on 11/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOne_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
