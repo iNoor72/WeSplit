fb1112f40dbc2a295dc2785db8b4c23bd7695723
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-04-13 21:39:26 +0200

    Designed the UI with Form, Pickers, and Texts inside a NavigationView. Created the logic, and finally it works 🎉.

--- ProjectOne-WeSplit/ContentView.swift
+++ ProjectOne-WeSplit/ContentView.swift
@@ -8,10 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var billAmount: String = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    var totalAmountPerPerson : Double {
+        let totalAmount = Double(billAmount) ?? 0
+        let peopleNumber = Double(numberOfPeople + 2)
+        let tipSelected = Double(tipPercentages[tipPercentage])
+        
+        let amountWithTip = (totalAmount * tipSelected/100) + totalAmount
+        return amountWithTip / peopleNumber
+    }
+    
+    let tipPercentages = [10,15,20,25]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        Text("Welcome to my 1st project in 100 days of SwiftUI!")
+        NavigationView{
+            Form {
+                TextField("Enter the total amount", text: $billAmount)
+                Picker("Number of people", selection: $numberOfPeople){
+                    ForEach(2..<100){
+                        Text("\($0) people")
+                    }
+                }
+                
+                Section(header: Text("How much tip you want to add?")) {
+                    Picker("Tip Amount:", selection: $tipPercentage){
+                        ForEach(0..<tipPercentages.count){
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$ \(totalAmountPerPerson, specifier: "%.2f")")
+                }
+            }
+//            .navigationTitle("WeSplit")
+            .navigationBarTitle("WeSplit")
+        }
     }
 }
 
